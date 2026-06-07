d5fb8537df435781da31829b5aca4d4882463b3c
saicharithajayini07-design <saicharithajayini07@gmail.com>
2026-06-07 13:57:55 -0500

    Added Employee class

---
+++ Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+
+    private int id;
+    private String name;
+    private String department;
+
+    public Employee(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public void displayEmployee() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+    }
+}
